e2d14e0a163f4188d85ac2282b168c8adbd95f10
mcsrk <49255542+mcsrk@users.noreply.github.com>
2022-06-21 21:53:41 -0500

    send req to translate

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import React from "react";
 
 import TextBox from "./components/TextBox";
@@ -14,63 +14,70 @@ const App = () => {
   const [textToTranslate, setTextToTranslate] = useState("");
   const [translatedText, setTranslatedText] = useState("");
 
-  const translate = async () => {
-    console.log("trainsalte");
-    const data = {
-      textToTranslate,
-      outputLanguage,
-      inputLanguage,
-    };
-    const response = await axios.get("http://localhost:8000/translation", {
-      params: data,
-    });
-    console.log("response", response);
-    setTranslatedText(response.data);
-  };
-
-  const handleClick = () => {
+  const handleClickTranslate = async () => {
+    console.log("translate");
     console.log({ textToTranslate });
+    const encodedString = Buffer.from(textToTranslate).toString("base64");
+    console.log({ encodedString });
+
+    axios({
+      method: "post",
+      url: "http://localhost:4567/translate",
+      data: textToTranslate,
+      headers: { "Content-Type": "text/html" },
+    })
+      .then((response) => {
+        //handle success
+        console.log(response);
+        setTranslatedText(response.data);
+      })
+      .catch((response) => {
+        //handle error
+        console.log(response);
+      });
   };
 
   return (
-    <div className="app">
-      {!showModal && (
-        <>
-          <TextBox
-            style="input"
-            setShowModal={setShowModal}
-            selectedLanguage={inputLanguage}
-            setTextToTranslate={setTextToTranslate}
-            textToTranslate={textToTranslate}
-            setTranslatedText={setTranslatedText}
-          />
-          <button className="traducir-button" onClick={handleClick}>
-            Traducir
-          </button>
-          <TextBox
-            style="output"
-            setShowModal={setShowModal}
-            selectedLanguage={outputLanguage}
-            translatedText={translatedText}
-          />
-          {/* <div className="button-container" onClick={translate}>
+    <>
+      <div className="app">
+        {!showModal && (
+          <>
+            <TextBox
+              style="input"
+              setShowModal={setShowModal}
+              selectedLanguage={inputLanguage}
+              setTextToTranslate={setTextToTranslate}
+              textToTranslate={textToTranslate}
+              setTranslatedText={setTranslatedText}
+            />
+            <button className="traducir-button" onClick={handleClickTranslate}>
+              Traducir
+            </button>
+            <TextBox
+              style="output"
+              setShowModal={setShowModal}
+              selectedLanguage={outputLanguage}
+              translatedText={translatedText}
+            />
+            {/* <div className="button-container" onClick={translate}>
             <Button />
           </div> */}
-        </>
-      )}
-      {showModal && (
-        <Modal
-          showModal={showModal}
-          setShowModal={setShowModal}
-          chosenLanguage={
-            showModal === "input" ? inputLanguage : outputLanguage
-          }
-          setChosenLanguage={
-            showModal === "input" ? setInputLanguage : setOutputLanguage
-          }
-        />
-      )}
-    </div>
+          </>
+        )}
+        {showModal && (
+          <Modal
+            showModal={showModal}
+            setShowModal={setShowModal}
+            chosenLanguage={
+              showModal === "input" ? inputLanguage : outputLanguage
+            }
+            setChosenLanguage={
+              showModal === "input" ? setInputLanguage : setOutputLanguage
+            }
+          />
+        )}
+      </div>
+    </>
   );
 };
 
--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SelectDropDown from "./SelectDropDown";
+import Highlight from "react-highlight";
 
 const TextBox = ({
   style,
@@ -10,30 +11,35 @@ const TextBox = ({
   translatedText,
   setTranslatedText,
 }) => {
-  const handleClick = () => {
+  const handleClickDelete = () => {
     setTextToTranslate("");
     setTranslatedText("");
   };
   return (
-    <div className={style}>
-      <SelectDropDown
-        style={style}
-        setShowModal={setShowModal}
-        selectedLanguage={selectedLanguage}
-      />
-      <textarea
-        disabled={style === "output"}
-        className={style}
-        placeholder={style == "input" ? "Enter text" : "Translation"}
-        onChange={(e) => setTextToTranslate(e.target.value)}
-        value={style === "input" ? textToTranslate : translatedText}
-      />
-      {style === "input" && (
-        <div className="delete" onClick={handleClick}>
-          ˟
-        </div>
-      )}
-    </div>
+    <>
+      <div className={style}>
+        <SelectDropDown
+          style={style}
+          setShowModal={setShowModal}
+          selectedLanguage={selectedLanguage}
+        />
+
+        <textarea
+          disabled={style === "output"}
+          className={style}
+          placeholder={style == "input" ? "Ingrese código HTMl" : "Traducción"}
+          onChange={(e) => setTextToTranslate(e.target.value)}
+          value={style === "input" ? textToTranslate : translatedText}
+        />
+        {/* <Highlight language="javascript"></Highlight> */}
+
+        {style === "input" && (
+          <div className="delete" onClick={handleClickDelete}>
+            ˟
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
